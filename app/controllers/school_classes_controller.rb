06ff05ddb049e5b2cf015718e082382b81a18742
Elaine Anderson <eparie@bu.edu>
2018-08-02 02:02:32 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,10 @@
 class SchoolClassesController < ActionController::Base
 
+
+	def new
+		@school_class = SchoolClass.new
+	end
+
   def create
     @school_class = SchoolClass.new(school_class_params(:title, :room_number))
 @school_class.save
