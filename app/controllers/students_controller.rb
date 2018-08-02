6437dd6aa5165714be6c09f0b1b674d930ef1eb7
Elaine Anderson <eparie@bu.edu>
2018-08-02 02:38:14 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,7 +20,7 @@ def show
 
     def update
       @student = Student.find(params[:id])
-  @student.update(student_params(:first_name, :last_name))
+  @student.update(students_params(:first_name, :last_name))
   redirect_to student_path(@student)
     end
 
