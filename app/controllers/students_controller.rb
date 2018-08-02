a1a1824a6ea507103f96ae51545ffce05f5a1c36
Elaine Anderson <eparie@bu.edu>
2018-08-02 02:33:09 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ActionController::Base
   def new
 		@student = Student.new
 	end
-  
+
   def create
     @student = Student.new(students_params(:first_name, :last_name))
     @student.save
